9c398ba429f86390b837a7bb13b6c170eb952951
Monica Lee <mylee.715@gmail.com>
2019-06-07 17:31:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,15 +10,36 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+ var price = Math.floor(Math.random() * 101);
+ var itemObj = {[item]: price};
+ cart.push(itemObj);
+ console.log(`${item} has been added to your cart.`);
+ return cart;
 }
 
 function viewCart() {
-  // write your code here
+  var cartItems = [];
+  if (cart.length === 0) {
+    return `Your shopping cart is empty.`;
+  } else if (cart.length === 1) {
+    console.log(`In your cart, you have ${Object.keys(cart[0])} at $${Object.values(cart[0])}.`);
+  } else if (cart.length === 2) {
+  console.log(`In your cart, you have ${Object.keys(cart[0])} at $${Object.values(cart[0])} and ${Object.keys(cart[1])} at $${Object.values(cart[1])}.`);
+  } else {
+      for (var i = 0; i < cart.length-1; i++) {
+        cartItems.push(`In your cart, you have ${Object.keys(cart[i])} at $${Object.values(cart[i])})`);
+      }
+}
 }
 
+
 function total() {
-  // write your code here
+  var cartItemTotal = [];
+  let i = 0;
+  while (i < cart.length) {
+    cartItemTotal += cart[i][Object.values[cart]];
+  }
+total();
 }
 
 function removeFromCart(item) {
